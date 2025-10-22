e917b4e9926f5d30714b5c1464be14e1b659c892
Aryan-Thapa-Np <aryanthapa583@gmail.com>
2025-10-22 11:38:01 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,68 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+
+
+    const toggle_Sidebar = document.querySelector('.toggle-hamburger');
+    const sidebar = document.querySelector('.dropbar');
+
+    let toggleCheck = true;
+    toggle_Sidebar.addEventListener("click", () => {
+
+        if (toggleCheck) {
+            toggleCheck = false;
+            toggle_Sidebar.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
+        } else {
+
+            toggle_Sidebar.innerHTML = `<i class="fa-solid fa-bars"></i>`;
+            toggleCheck = true;
+        }
+
+        sidebar.classList.toggle("show");
+
+    });
+
+
+    const customCursorOutline = document.querySelector('.curosr-outline');
+    const customCursorDot = document.querySelector('.cursor-dot');
+
+
+
+    window.addEventListener("mousemove", (e) => {
+       
+
+        customCursorDot.style.top = `${e.clientY}px`;
+        customCursorDot.style.left = `${e.clientX}px`;
+
+
+
+
+
+
+        function updateMouseLocation() {
+
+
+
+
+            customCursorOutline.animate(
+                {
+                    top: `${e.clientY}px`,
+                    left: `${e.clientX}px`
+                },
+                {
+                    duration: 50,
+                    fill: "forwards"
+
+                }
+            );
+
+
+        }
+
+
+        updateMouseLocation();
+
+
+    });
+
+
+});
